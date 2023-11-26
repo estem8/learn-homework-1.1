5f01c5a92feaa13845ce9c587003838298add886
estem8 <awards@mail.ru>
2023-11-26 14:43:28 +0300

    Fix

--- server.py
+++ server.py
@@ -1,21 +1,40 @@
 from flask import Flask, request, render_template
 import requests
-from weather import get_weather
+from weather import *
+from dotenv import load_dotenv
+from os.path import join, dirname
+import os
+
 
 app = Flask(__name__)
 
+def get_from_env(key):
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    return os.environ.get(key)
 
+def generate_template()-> dict:
+    """Генератор data для render template"""    
+    data = get_weather()
+    data_by_ip = get_geo_by_ip()
+    city_by_cord = get_city_name_by_coord()
+    refactoring_data = {
+        'my_ip':get_my_ip(),
+        'longitude': data['coord']['lon'],
+        'latitude': data['coord']['lat'],
+        'country': data_by_ip['country'],
+        'city': data_by_ip['city'],
+        'weather': data['weather'][0]['description'],
+        'city_by_cord': city_by_cord,
 
+    }
 
-def generate_template():
-    # data = get_weather()
-    return get_weather()
+    return refactoring_data
 
 
 @app.route('/')
 def hello():
     data = generate_template()
-
     return render_template('index.html', data = data)
 
 if __name__=='__main__':
--- weather.py
+++ weather.py
@@ -1,27 +1,43 @@
 import requests
 from setting import weather_api_key
+from geopy.geocoders import Nominatim
 
-def get_my_ip():
+
+def get_my_ip() -> str:
     url='https://ident.me'
     my_ip = requests.get(url=url)
     return my_ip.text
     
-def get_geo_by_ip():
+def get_geo_by_ip() -> dict:
     url='http://ip-api.com/json/'
     geo_data = requests.get(url, get_my_ip())
-    return geo_data.text
+    return geo_data.json()
 
-def get_weather():
+def get_weather() -> dict:
     weather_url = 'http://api.openweathermap.org/data/2.5/weather'
+    city = get_geo_by_ip()['city']
     params = {
-        'q': 'Moscow',
+        'q': city,
         'appid': weather_api_key
         }
     result = requests.get(weather_url,params=params) 
     weather_data = result.json()
     return weather_data
 
+def get_city_name_by_coord():
+    data = get_geo_by_ip()
+    latitude = data['lat']
+    longitude = data['lon']
+    geolocator = Nominatim(user_agent="my_geocoder")
+    location = geolocator.reverse((latitude, longitude), language='ru')
+    if location and 'address' in location.raw:
+        city_name = location.raw['address'].get('city', '')
+        return city_name
+    else:
+        return None
 
+def main():
+    pass
 
 if __name__=='__main__':
-    get_weather()
+    main()

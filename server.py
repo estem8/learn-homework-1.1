058712cc1ad986addf7b42686d97f9ca75f54049
estem8 <awards@mail.ru>
2023-11-28 22:28:12 +0300

    Реализация класса

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, render_template
-import requests
-from weather import *
+
 from dotenv import load_dotenv
 from os.path import join, dirname
 import os
+
+from lvl_one import generate_template
+from weather_as_class import Weather
+
 
 
 app = Flask(__name__)
@@ -13,29 +16,21 @@ def get_from_env(key):
     load_dotenv(dotenv_path)
     return os.environ.get(key)
 
-def generate_template()-> dict:
-    """Генератор data для render template"""    
-    data = get_weather()
-    data_by_ip = get_geo_by_ip()
-    city_by_cord = get_city_name_by_coord()
-    refactoring_data = {
-        'my_ip':get_my_ip(),
-        'longitude': data['coord']['lon'],
-        'latitude': data['coord']['lat'],
-        'country': data_by_ip['country'],
-        'city': data_by_ip['city'],
-        'weather': data['weather'][0]['description'],
-        'city_by_cord': city_by_cord,
-
-    }
-
-    return refactoring_data
 
 
-@app.route('/')
-def hello():
+
+@app.route('/lvl_one')
+def lvl_one():
     data = generate_template()
     return render_template('index.html', data = data)
+
+
+@app.route('/lvl_two')
+def lvl_two():
+    data = Weather.create_data_with_my_ip()
+    print(data.__dict__)
+    return render_template('index.html', data = data)
+
 
 if __name__=='__main__':
     app.run(debug=True)
